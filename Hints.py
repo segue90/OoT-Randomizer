a39c8e6c093894f52bff0c74b346d3e587a5fb5c
Tobias Gies <tobias@tobiasgies.de>
2023-09-12 23:11:25 +0200

    Add coop3 hint distro

--- Hints.py
+++ Hints.py
@@ -49,7 +49,7 @@ defaultHintDists: list[str] = [
     'bingo.json',
     'chaos.json',
     'chaos_no_goal.json',
-    'coop2.json',
+    'coop3.json',
     'ddr.json',
     'important_checks.json',
     'league.json',
