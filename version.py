75971367693aec52f2eba0df3d086954b210b0be
TreZc0 <trezc0@gmx.de>
2023-11-08 04:27:09 +0100

    remove version changes

--- version.py
+++ version.py
@@ -1,14 +1,14 @@
 __version__ = '7.1.202'
 
 # This is a supplemental version number for branches based off of main dev.
-supplementary_version = 3
+supplementary_version = 0
 
 # Pick a unique identifier byte for your fork if you are intending to have a long-lasting branch.
 # This will be 0x00 for main releases and 0x01 for main dev.
-branch_identifier = 0x9c
+branch_identifier = 0x01
 
 # URL to your branch on GitHub.
-branch_url = 'https://github.com/TreZc0/OoT-Randomizer/tree/Dev'
+branch_url = 'https://github.com/TestRunnerSRL/OoT-Randomizer/tree/Dev'
 
 # This is named __version__ at the top for compatability with older versions trying to version check.
 base_version = __version__
