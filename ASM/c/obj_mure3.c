17ae3a5b53d1043a80ec2be95943db9900981503
Rob Realmuto <rrealmuto@gmail.com>
2024-02-06 18:26:03 -0500

    Fix flag check for goron pot, rupee circles, shadow pot

--- ASM/c/obj_mure3.c
+++ ASM/c/obj_mure3.c
@@ -6,12 +6,12 @@ z64_actor_t* Obj_Mure3_RupeeCircle_DropRupee(z64_actor_t* this, z64_xyzf_t* pos,
     xflag_t flag = Actor_GetAdditionalData(this)->flag;
     if(flag.all) {
         flag.subflag = index;
-    }
-    if(!Get_NewOverrideFlag(&flag)) {
-        drop_collectible_override_flag = flag;
-        z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible2(&z64_game, pos, params);
-        z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
-        return spawned;
+        if(!Get_NewOverrideFlag(&flag)) {
+            drop_collectible_override_flag = flag;
+            z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible2(&z64_game, pos, params);
+            z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
+            return spawned;
+        }
     }
     return (z64_actor_t*)z64_Item_DropCollectible2(&z64_game, pos, params);
 }
--- ASM/c/ovl_bg_spot18_basket.c
+++ ASM/c/ovl_bg_spot18_basket.c
@@ -5,12 +5,14 @@
 z64_actor_t* BgSpot18Basket_BombDropHook(z64_actor_t* this, z64_xyzf_t* pos, uint32_t index)
 {
     xflag_t flag = Actor_GetAdditionalData(this)->flag;
-    flag.subflag = index;
-    if(flag.all && !Get_NewOverrideFlag(&flag)) {
-        drop_collectible_override_flag = flag;
-        z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
-        z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
-        return spawned;
+    if(flag.all) {
+        flag.subflag = index;
+        if(!Get_NewOverrideFlag(&flag)) {
+            drop_collectible_override_flag = flag;
+            z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
+            z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
+            return spawned;
+        }
     }
     return (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 4);
 }
@@ -18,25 +20,31 @@ z64_actor_t* BgSpot18Basket_BombDropHook(z64_actor_t* this, z64_xyzf_t* pos, uin
 z64_actor_t* BgSpot18Basket_RupeeDropHook(z64_actor_t* this, z64_xyzf_t* pos, uint32_t index)
 {
     xflag_t flag = Actor_GetAdditionalData(this)->flag;
-    flag.subflag = index + 3;
-    if(flag.all && !Get_NewOverrideFlag(&flag)) {
-        drop_collectible_override_flag = flag;
-        z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
-        z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
-        return spawned;
+    if(flag.all) {
+        flag.subflag = index + 3;
+        if(!Get_NewOverrideFlag(&flag)) {
+            drop_collectible_override_flag = flag;
+            z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
+            z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
+            return spawned;
+        }
     }
+    
     return (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
 }
 
 z64_actor_t* BgSpot18Basket_Heartpiecerupee_DropHook(z64_actor_t* this, z64_xyzf_t* pos, uint16_t params)
 {
     xflag_t flag = Actor_GetAdditionalData(this)->flag;
-    flag.subflag = params + 5;
-    if(flag.all && !Get_NewOverrideFlag(&flag)) {
-        drop_collectible_override_flag = flag;
-        z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
-        z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
-        return spawned;
+    if(flag.all) {
+        flag.subflag = params + 5;
+        if(!Get_NewOverrideFlag(&flag)) {
+            drop_collectible_override_flag = flag;
+            z64_actor_t* spawned = (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, 0);
+            z64_bzero(&drop_collectible_override_flag, sizeof(drop_collectible_override_flag));
+            return spawned;
+        }
     }
+    
     return (z64_actor_t*)z64_Item_DropCollectible(&z64_game, pos, params);
 }
