0ea5672b660e6fe6a10be416dc8b9feeafbe6359
rrealmuto <rrealmuto@gmail.com>
2024-06-22 00:18:50 -0400

    Update ASM/src/bg_gate_shutter.asm
    Co-authored-by: Fenhl <fenhl@fenhl.net>

--- ASM/src/bg_gate_shutter.asm
+++ ASM/src/bg_gate_shutter.asm
@@ -3,7 +3,7 @@
 ; Put the flag into t9 because it will be checked against 0 when we return
 ; we can use t8
 bg_gate_shutter_open_hack:
-    li  t8, OPEN_KAKARIKO
-    lb  t8, 0x00(t8) ; read the value of the OPEN_KAKARIKO setting. If we set it to 2 then it's always open
-    jr  ra
+    li      t8, OPEN_KAKARIKO
+    lb      t8, 0x00(t8) ; read the value of the OPEN_KAKARIKO setting. If we set it to 2 then it's always open
+    jr      ra
     andi    t9, t8, 0x02
